db82df516a044198c6aeabebb80e551beaec18e5
Muhammad Javaid <jvdbms2131@gmail.com>
2024-03-18 11:58:21 +0500

    update

---
+++ index.py
@@ -0,0 +1,4 @@
+print('pakistan zindabad')
+print("I love my country")
+print ("My Name is Rana Muhammad Javaid")
+print ("diciplint is what you hate to do but do it like you love it do you will be the successfull man in the world")
